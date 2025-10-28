09e7054dbc59906c960f2cedb2db76b14195c1a2
Abubakar Mi <abubakarmi131@gmail.com>
2025-10-28 13:15:57 +0000

    revert this change

--- src/app/sections/skills.tsx
+++ src/app/sections/skills.tsx
@@ -44,7 +44,7 @@ const NextjsIcon = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} viewBox="-11.5 -10.23174 23 20.46348" fill="none" xmlns="http://wwwডিয়া.w3.org/2000/svg">
+    <svg {...props} viewBox="-11.5 -10.23174 23 20.46348" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="0" cy="0" r="2.05" fill="#61DAFB"/>
         <g stroke="#61DAFB" strokeWidth="1" fill="none">
             <ellipse rx="11" ry="4.2"/>
